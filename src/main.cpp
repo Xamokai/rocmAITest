df3f682fe8a3694df87d9981e28574046f4a25cb
Xamokai <maxi009@online.de>
2023-10-12 13:15:03 +0200

    train funktion ++ checkBoard

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,67 @@
 #include "utils.h"
 #include <array>
 
-__global__ void train(int i, int nets, arr* d_weights, arr* d_biases){
+struct arr{
+  float* ptr;
+  unsigned long size;
+
+  __device__ __host__ float operator[](unsigned long i){
+    return this->ptr[i];
+  }
+
+  __device__ __host__ arr slice(unsigned long i, unsigned long j){
+    if(i < j) {
+      return arr{nullptr, 0};
+    }
+
+    return arr{(this->ptr)+i, i - j};
+  }
+};
+
+__device__ int checkBoard(int board[9]){
+  int ret = 0;
+  for(int i = 0; i < 9; i+=3){
+    ret = ((board[0 + i] == board[1 + i]) && (board[1 + i] == board[2 + i])) * board[0 + i];
+  }
+  for(int i = 0; i < 3; i++){
+    ret = ((board[0 + i] == board[3 + i]) && (board[3 + i] == board[6 + i])) * board[0 + i];
+  }
+  ret = ((board[0] == board[4]) && (board[4] == board[8])) * board[0];
+  ret = ((board[2] == board[4]) && (board[4] == board[6])) * board[6];
+  return ret;
+}
+
+
+__global__ void train(int i, int nets, arr d_weights, arr d_biases){
   unsigned long a = threadIdx.x + blockIdx.x * blockDim.x;
-  unsigned long b = a + i;
+  unsigned long b = (a + i) & (nets-1);
+  if(a==b)
+    b+=1;
+
+  arr slice_a_w = d_weights.slice(a * d_weights.size, d_weights.size);
+  arr slice_b_w = d_weights.slice(b * d_weights.size, d_weights.size);
+
+  arr slice_a_b = d_biases.slice(a * d_biases.size, d_biases.size);
+  arr slice_b_b = d_biases.slice(b * d_biases.size, d_biases.size);
+
+  int board[9] = {0};
+  
+  int check = 0;
+  for(int i = 0; i < 9 && checkBoard(board) == 0; i++){
+
+    check = checkBoard(board);
+  }
+
+  arr from_w;
+  arr to_w;
+  if(check == 1){
+    from_w = slice_a_w;
+    to_w = slice_b_w;
+  }else {
+    from_w = slice_b_w;
+    to_w = slice_a_w;
+  }
+
 
 }
 
@@ -20,11 +78,14 @@ int main(){
     num_weights += layout[i] * layout[i+1];
   }
   
-  array<float> d_weights;
-  float* d_biases;
+  arr d_weights;
+  arr d_biases;
 
-  HIP_CHECK(hipMalloc(&d_weights, sizeof(float) * num_weights * num_nets));
-  HIP_CHECK(hipMalloc(&d_biases, sizeof(float) * num_biases * num_nets));
+  HIP_CHECK(hipMalloc(&d_weights.ptr, sizeof(float) * num_weights * num_nets));
+  HIP_CHECK(hipMalloc(&d_biases.ptr, sizeof(float) * num_biases * num_nets));
+
+  d_weights.size = num_weights;
+  d_biases.size = num_biases;
 
   int iterations = 200;
   for(int i = 0; i < iterations; i++){
